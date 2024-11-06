b1d7b22d0fe5a7c37bcfd090f4feb5b0d5835a7c
pedro-makoski <148820090+pedro-makoski@users.noreply.github.com>
2024-11-06 17:54:26 -0300

    melhorias scroll

--- scripts/scroll.js
+++ scripts/scroll.js
@@ -1,16 +1,19 @@
-let [menuguide, scrollmenu, width, backstrong, backnotstrong, pos] = ['.menu-guide', '.scroll_menu', window.innerWidth, 'var(--cor-contraste)', 'var(--cor-contraste-do-contraste)', 1];
+let [menuguide, scrollmenu, backstrong, backnotstrong, pos] = ['.menu-guide', '.scroll_menu', 'var(--cor-contraste)', 'var(--cor-contraste-do-contraste)', 1];
+const elemento = document.querySelector(scrollmenu);
+
+let width = elemento.getBoundingClientRect().width;
+
+let before; 
 
 const button_left = document.querySelector('.button-move-scroll .toleft');
 const button_right = document.querySelector('.button-move-scroll .toright');
 
-let before = demonstrate_scroll(scrollmenu, width, menuguide, backstrong, backnotstrong, pos);
-
-const elemento = document.querySelector(scrollmenu);
+[before, pos] = demonstrate_scroll(scrollmenu, width, menuguide, backstrong, backnotstrong, pos);
 
 let timer;
 
 window.addEventListener('resize', () => {
-    width = window.innerWidth;
+    width = elemento.getBoundingClientRect().width;
     goToActualPos(scrollmenu, width, pos);
 });
 
