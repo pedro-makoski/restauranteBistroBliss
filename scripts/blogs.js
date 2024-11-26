cd8557671243af45a4760aaaf0486ac244226660
pedro-makoski <148820090+pedro-makoski@users.noreply.github.com>
2024-11-26 13:26:49 -0300

    melhorias

--- scripts/blogs.js
+++ scripts/blogs.js
@@ -3,6 +3,9 @@ const blogs_place = document.querySelector('#blogs .components')
 mudarLayout('./scripts/blog-itens.json', TEXTO_DE_SUMICO, false, null, false, () => {}, '<article class="blog"><a href="{path}" target="_self" rel="next"><div class="title"><h3>{title}</h3><p class="date">{date}</p></div><img src="{imgUrl}"></a></article>', blogs_place, [], false, 0);
 
 const input = document.getElementById("search");
+if(input !== undefined) {
+    mudarLayout('./scripts/blog-itens.json', TEXTO_DE_SUMICO, true, input, false, () => {}, '<article class="blog"><a href="{path}" target="_self" rel="next"><div class="title"><h3>{title}</h3><p class="date">{date}</p></div><img src="{imgUrl}"></a></article>', blogs_place, ["title"], false, 0);
+}
 
 input.addEventListener('input', () => {
     mudarLayout('./scripts/blog-itens.json', TEXTO_DE_SUMICO, true, input, false, () => {}, '<article class="blog"><a href="{path}" target="_self" rel="next"><div class="title"><h3>{title}</h3><p class="date">{date}</p></div><img src="{imgUrl}"></a></article>', blogs_place, ["title"], false, 0);
